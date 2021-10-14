cd14b725f6bedcf17699396daa29154221745dea
Salsabillaayuni <salsabillaayuni31@gmail.com>
2021-10-14 00:08:18 -0700

    prak tgs

--- app/Views/view_register.php
+++ app/Views/view_register.php
@@ -1,9 +1,11 @@
-<?= $this->extend('template');?>
-<?= $this->section('content');?>
+<?= $this->extend('layouts/template'); ?>
+
+<?= $this->section('content'); ?>
+
 <body class="hold-transition register-page">
 <div class="register-box">
   <div class="register-logo">
-    <a href="/assets/adminlte/index2.html"><b>Admin</b>LTE</a>
+    <a href="assets/adminlte/index2.html"><b>Admin</b>LTE</a>
   </div>
 
   <div class="card">
@@ -28,18 +30,14 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password" id="password" name="password">
+          <input type="password" class="form-control" placeholder="Password"id="password" name="password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
             </div>
           </div>
-        </div>
-        
-        <div class="row">
-         
           <!-- /.col -->
-          <div class="col-12">
+          <div class="col-4">
             <button type="submit" class="btn btn-primary btn-block">Register</button>
           </div>
           <!-- /.col -->
@@ -51,4 +49,5 @@
   </div><!-- /.card -->
 </div>
 <!-- /.register-box -->
-<?= $this->endSection();?>
+
+<?= $this->endSection() ?>
